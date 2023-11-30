9196f24146dce20ff92ff33743daea691302b0ef
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-11-30 01:07:56 +0000

    Adding Pir Sensor, project V0.2

--- ArduinoRealTimeProject.ino
+++ ArduinoRealTimeProject.ino
@@ -1,19 +1,21 @@
 #include <SPI.h>
 #include <WiFiNINA.h>
-#include <ArduinoHttpClient.h>
 #include "config.h"
 #include <Arduino_MKRIoTCarrier.h>
-
-char ssid[] = SECRET_SSID; //  network SSID 
-char pass[] = SECRET_PASS; //  network password
+#include <Firebase_Arduino_WiFiNINA.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+char ssid[] = SECRET_SSID; // Network SSID 
+char pass[] = SECRET_PASS; // Network password
 
 MKRIoTCarrier carrier;
+FirebaseData fbdo;
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", 0); // NTP server address and time offset (adjust time offset as needed)
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {
-    ; // waiting for Serial port to connect.
-  }
+  while (!Serial); // Wait for the serial port to connect
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, pass);
@@ -23,45 +25,50 @@ void setup() {
   }
   Serial.println("Connected to Wi-Fi");
 
+  // Initialize NTP Client
+  timeClient.begin();
+  timeClient.setTimeOffset(3600); // Set the time offset for your timezone
+
+  // Initialize Firebase
+  Firebase.begin(FIREBASE_HOST, FIREBASE_SECRET, ssid, pass);
+  carrier.withCase();
   carrier.begin();
 }
 
-const char* firebaseHost = FIREBASE_HOST;
-const int httpPort = 443;
-WiFiSSLClient wifiClient;
-HttpClient client = HttpClient(wifiClient, firebaseHost, httpPort);
-String contentType = "application/json";
+void sendDataToFirebase() {
+  timeClient.update();
+  unsigned long timestamp = timeClient.getEpochTime(); // Get current Unix timestamp
 
-
-void sendDataToFirebase(String path, String data) {
-  client.beginRequest();
-  client.post(path, contentType, data);
-  client.endRequest();
-  
-  // Read the response and print it
-  int statusCode = client.responseStatusCode();
-  String response = client.responseBody();
-  Serial.print("Status code: ");
-  Serial.println(statusCode);
-  Serial.print("Response: ");
-  Serial.println(response);
-}
-
-void loop() {
   // Read sensor data
   float temperature = carrier.Env.readTemperature();
   float humidity = carrier.Env.readHumidity();
   float pressure = carrier.Pressure.readPressure();
-  int moisture = analogRead(A0); =
-  // Convert sensor data to JSON string
-  String data = "{\"temperature\": " + String(temperature) + 
-                ", \"humidity\": " + String(humidity) + 
-                ", \"pressure\": " + String(pressure) + 
-                ", \"moisture\": " + String(moisture) + "}";
-  String firebasePath = "/sensors.json"; // .json is important
+  int moisture = analogRead(A0);
+  float gasResistor = carrier.AirQuality.readGasResistor();
+  float volatileOrganicCompounds = carrier.AirQuality.readVOC();
+  float co2 = carrier.AirQuality.readCO2();
+
+
+  // Create a unique path for each set of readings using a timestamp
+  String path = "sensorReadings/" + String(timestamp);
 
   // Send data to Firebase
-  sendDataToFirebase(firebasePath, data);
+  // Firebase.setFloat(fbdo, path + "/temperature", temperature);
+  // Firebase.setFloat(fbdo, path + "/humidity", humidity);
+  // Firebase.setFloat(fbdo, path + "/pressure", pressure);
+  // Firebase.setInt(fbdo, path + "/moisture", moisture);
+}
 
-  delay(10000); 
+void loop() {
+int pirState = analogRead(A6); // Read PIR value
+Serial.println(pirState);
+  if (pirState == HIGH) {
+    // Motion detected
+    Serial.println("Motion detected!");
+  } else {
+    // No motion detected
+    Serial.println("No motion.");
+  }
+  sendDataToFirebase();
+  delay(1000); // Adjust the delay as needed
 }
