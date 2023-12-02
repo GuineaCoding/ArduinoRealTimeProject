90d087a56a06dc69ad3507276db777abfa3e5a44
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-12-02 00:50:51 +0000

    project V0.4

--- ArduinoRealTimeProject.ino
+++ ArduinoRealTimeProject.ino
@@ -6,6 +6,7 @@
 #include <WiFiUdp.h>
 #include <NTPClient.h>
 #include <time.h>
+#include "icons.h"
 
 
 char ssid[] = SECRET_SSID; // Network SSID
@@ -17,14 +18,11 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 0); // NTP server address and time offset (adjust time offset as needed)
 
 // PIR Sensor
-<<<<<<< HEAD
 const int pirPin = A6; 
-=======
-const int pirPin = A6; // Use a different pin if A1 is occupied
->>>>>>> 1790bc78ecc8b4e84b67b9f3a0cff98a11fbfd1c
 bool pirState = LOW;
 uint32_t colorGreen = carrier.leds.Color(0, 50, 0); // Green
 uint32_t colorRed = carrier.leds.Color(50, 0, 0);   // Red
+
 
 bool motionDetected = false;
 unsigned long motionDetectedTime = 0;
@@ -77,59 +75,80 @@ void sendDataToFirebase() {
   String path = "sensorReadings/" + String(timestamp);
 
   // Send data to Firebase
-  // Firebase.setFloat(fbdo, path + "/temperature", temperature);
-  // Firebase.setFloat(fbdo, path + "/humidity", humidity);
-  // Firebase.setFloat(fbdo, path + "/pressure", pressure);
-  // Firebase.setInt(fbdo, path + "/moisture", moisture);
+  Firebase.setFloat(fbdo, path + "/temperature", temperature);
+  Firebase.setFloat(fbdo, path + "/humidity", humidity);
+  Firebase.setFloat(fbdo, path + "/pressure", pressure);
+  Firebase.setInt(fbdo, path + "/moisture", moisture);
 }
 
 
 
 void loop() {
-  sendDataToFirebase();
-  carrier.Buttons.update();
+  // sendDataToFirebase();
 
+  //   if (Firebase.getBool(fbdo, "ledCommand/red")) {
+  //   if (fbdo.dataType() == "boolean") {
+  //     bool red = fbdo.boolData();
+  //     if (red) {
+  //       carrier.display.fillScreen(0x07E0);
+  //     } else {
+  //       carrier.display.fillScreen(0x0000FF);
+  //     }
+  //     carrier.leds.show();
+  //   }
+  // }
+  carrier.Buttons.update(); // Update the status of the touch buttons
+
+  // Check for the "UP" gesture
   if (carrier.Light.gestureAvailable()) {
     uint8_t gesture = carrier.Light.readGesture();
     Serial.println(gesture);
 
-    if (gesture == UP || carrier.Buttons.onTouchDown(TOUCH0)) {
+    if (gesture == UP) { // Replace APDS9960_DIR_UP with the appropriate constant for "up" gesture
+    carrier.Buzzer.beep(800, 20);
       displayDateTimeTempHumidity();
     }
   }
 
-  pirState = digitalRead(pirPin);
-  Serial.println(pirState);
-
-  if (pirState == HIGH) {
-    if (!motionDetected) {
-      motionDetected = true;
-      motionDetectedTime = millis();
-      // displayMessage("Warning! Movement detected.");
-      // carrier.leds.fill(colorRed, 0, 5);
-    }
-  } else {
-    if (motionDetected && millis() - motionDetectedTime > redLightDuration) {
-      motionDetected = false;
-      // displayMessage("No movement detected. Everything is good here.");
-      // carrier.leds.fill(colorGreen, 0, 5);
-    }
+  // Check if button 0 is pressed
+  if (carrier.Buttons.onTouchDown(TOUCH0)) {
+    carrier.Buzzer.beep(800, 20);
+    displayDateTimeTempHumidity();
   }
-  carrier.leds.show();
+
+  // pirState = digitalRead(pirPin);
+  // Serial.println(pirState);
+
+  // if (pirState == HIGH) {
+  //   if (!motionDetected) {
+  //     motionDetected = true;
+  //     motionDetectedTime = millis();
+  //     // displayMessage("Warning! Movement detected.");
+  //     carrier.leds.fill(colorRed, 0, 5);
+  //   }
+  // } else {
+  //   if (motionDetected && millis() - motionDetectedTime > redLightDuration) {
+  //     motionDetected = false;
+  //     // displayMessage("No movement detected. Everything is good here.");
+  //     carrier.leds.fill(colorGreen, 0, 5);
+  //   }
+  // }
+  // carrier.leds.show();
 
   delay(1000); 
 }
 
-void displayMessage(const char* message) {
-  Serial.println(message);
-  carrier.display.fillScreen(ST77XX_BLACK);
-  carrier.display.setCursor(30, 100);
-  carrier.display.setTextSize(2);
-  carrier.display.println(message);
-}
+// void displayMessage(const char* message) {
+//   Serial.println(message);
+//   carrier.display.fillScreen(ST77XX_BLACK);
+//   carrier.display.setCursor(30, 100);
+//   carrier.display.setTextSize(2);
+//   carrier.display.println(message);
+// }
 
 
 void displayDateTimeTempHumidity() {
+  carrier.display.setRotation(180);
   timeClient.update();
 
   // Get the current Unix timestamp and convert to time structure
@@ -151,8 +170,17 @@ void displayDateTimeTempHumidity() {
   carrier.display.setTextColor(0x001F);
   carrier.display.setTextSize(2);
 
-  carrier.display.setCursor(10, 20);
+  // Adjust the coordinates in setCursor to change position
+  carrier.display.setCursor(40, 25); // Adjust these values as needed
   carrier.display.println(currentDate);
-  carrier.display.setCursor(10, 50);
+
+  carrier.display.setCursor(30, 55); // Adjust these values as needed
   carrier.display.println(currentTime);
+
+  carrier.display.setCursor(30, 85); // Adjust these values as needed
+  carrier.display.drawBitmap(25, 70, m_thermostat, 48, 48, 0xDAC9);
+  carrier.display.println(String(temperature) + " C");
+
+  carrier.display.setCursor(30, 115); // Adjust these values as needed
+  carrier.display.println("Hum: " + String(humidity) + "%");
 }
