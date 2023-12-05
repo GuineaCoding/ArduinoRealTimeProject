7a3867c15e17a1ef18045228a2bc3d99352da74f
Andrian Barbulat <andrian.barbulat@gmail.com>
2023-12-05 00:24:09 +0000

    V0.6, comleted date, time, temp, humidity feature

--- ArduinoRealTimeProject.ino
+++ ArduinoRealTimeProject.ino
@@ -10,36 +10,37 @@
 #include <TimeLib.h>
 
 
-char ssid[] = SECRET_SSID; // Network SSID
-char pass[] = SECRET_PASS; // Network password
+char ssid[] = SECRET_SSID;  // Network SSID
+char pass[] = SECRET_PASS;  // Network password
 
 MKRIoTCarrier carrier;
 FirebaseData fbdo;
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP); 
+NTPClient timeClient(ntpUDP);
 
 // PIR Sensor
-const int pirPin = A6; 
+const int pirPin = A6;
 bool pirState = LOW;
-uint32_t colorGreen = carrier.leds.Color(0, 50, 0); // Green
-uint32_t colorRed = carrier.leds.Color(50, 0, 0);   // Red
+uint32_t colorGreen = carrier.leds.Color(0, 50, 0);  // Green
+uint32_t colorRed = carrier.leds.Color(50, 0, 0);    // Red
 
 
 bool motionDetected = false;
 unsigned long motionDetectedTime = 0;
-const unsigned long redLightDuration = 30000; // 30 seconds
-uint8_t gesture = carrier.Light.readGesture(); 
+const unsigned long redLightDuration = 30000;  // 30 seconds
+uint8_t gesture = carrier.Light.readGesture();
 
 
-unsigned long displayStartMillis = 0; // Global variable to store start time
-const unsigned long displayDuration = 20000; // Display duration in milliseconds (20 seconds)
-bool displayActive = false; // Flag to indicate whether the display is active
+unsigned long displayStartMillis = 0;         // Global variable to store start time
+const unsigned long displayDuration = 20000;  // Display duration in milliseconds (20 seconds)
+bool displayActive = false;                   // Flag to indicate whether the display is active
 
 
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial); // Wait for the serial port to connect
+  while (!Serial)
+    ;  // Wait for the serial port to connect
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, pass);
@@ -54,16 +55,16 @@ void setup() {
 
   // Initialize Firebase
   Firebase.begin(FIREBASE_HOST, FIREBASE_SECRET, ssid, pass);
-    // Initialize PIR sensor
+  // Initialize PIR sensor
   pinMode(pirPin, INPUT);
   carrier.withCase();
   carrier.begin();
 }
- 
- 
+
+
 void sendDataToFirebase() {
   timeClient.update();
-  unsigned long timestamp = timeClient.getEpochTime(); // Get current Unix timestamp
+  unsigned long timestamp = timeClient.getEpochTime();  // Get current Unix timestamp
 
   Serial.println("Time synchronized");
   // Read sensor data
@@ -74,9 +75,12 @@ void sendDataToFirebase() {
   float gasResistor = carrier.AirQuality.readGasResistor();
   float volatileOrganicCompounds = carrier.AirQuality.readVOC();
   float co2 = carrier.AirQuality.readCO2();
-  Serial.print("Gas Resistor: "); Serial.println(gasResistor);
-  Serial.print("VOCs: "); Serial.println(volatileOrganicCompounds);
-  Serial.print("CO2: "); Serial.println(co2);
+  Serial.print("Gas Resistor: ");
+  Serial.println(gasResistor);
+  Serial.print("VOCs: ");
+  Serial.println(volatileOrganicCompounds);
+  Serial.print("CO2: ");
+  Serial.println(co2);
 
   // Create a unique path for each set of readings using a timestamp
   String path = "sensorReadings/" + String(timestamp);
@@ -104,29 +108,29 @@ void loop() {
   //     carrier.leds.show();
   //   }
   // }
-  carrier.Buttons.update(); // Update the status of the touch buttons
+  carrier.Buttons.update();  // Update the status of the touch buttons
 
   // Check for the "UP" gesture
   if (carrier.Light.gestureAvailable()) {
     uint8_t gesture = carrier.Light.readGesture();
     Serial.println(gesture);
 
-    if (gesture == UP) { // 
-    carrier.Buzzer.beep(800, 20);
+    if (gesture == UP) {  //
+      carrier.Buzzer.beep(800, 20);
       displayDateTimeTempHumidity();
     }
   }
 
-    // Check if button 0 is pressed
+  // Check if button 0 is pressed
   if (carrier.Buttons.onTouchDown(TOUCH0)) {
     carrier.Buzzer.beep(800, 20);
     displayDateTimeTempHumidity();
   }
 
-      if (displayActive && (millis() - displayStartMillis >= displayDuration)) {
-        carrier.display.fillScreen(ST77XX_BLACK); // Turn the display black
-        displayActive = false; // Reset the display active flag
-    }
+  if (displayActive && (millis() - displayStartMillis >= displayDuration)) {
+    carrier.display.fillScreen(ST77XX_BLACK);  // Turn the display black
+    displayActive = false;                     // Reset the display active flag
+  }
 
   // pirState = digitalRead(pirPin);
   // Serial.println(pirState);
@@ -147,7 +151,7 @@ void loop() {
   // }
   // carrier.leds.show();
 
-  delay(1000); 
+  delay(1000);
 }
 
 // void displayMessage(const char* message) {
@@ -160,97 +164,100 @@ void loop() {
 
 
 void displayDateTimeTempHumidity() {
-    carrier.display.setRotation(180);
-    timeClient.update();
-    String greeting;
+  carrier.display.setRotation(180);
+  timeClient.update();
+  String greeting;
 
-    // Get the current Unix timestamp and convert to time structure
-    time_t rawtime = timeClient.getEpochTime();
-    struct tm *ti = localtime(&rawtime);
+  // Get the current Unix timestamp and convert to time structure
+  time_t rawtime = timeClient.getEpochTime();
+  struct tm* ti = localtime(&rawtime);
 
-    char currentDate[11]; // Buffer to store the formatted date
-    strftime(currentDate, sizeof(currentDate), "%Y-%m-%d", ti); // Format date as YYYY-MM-DD
+  char currentDate[11];  // Buffer to store the formatted date
+  strftime(currentDate, sizeof(currentDate), "%Y-%m-%d", ti);  // Format date as YYYY-MM-DD
 
-    String currentTime = timeClient.getFormattedTime();
+  String currentTime = timeClient.getFormattedTime();
 
-    float temperature = carrier.Env.readTemperature();
-    float humidity = carrier.Env.readHumidity();
+  float temperature = carrier.Env.readTemperature();
+  float humidity = carrier.Env.readHumidity();
 
-    // Display dimensions and bitmap size
-    int displayWidth = carrier.display.width();
-    int displayHeight = carrier.display.height();
-    int bitmapSize = 48; // Size of the  images
-    int spacing = 10; // Spacing between elements
+  // Display dimensions and bitmap size
+  int displayWidth = carrier.display.width();
+  int displayHeight = carrier.display.height();
+  int bitmapSize = 32;  // Size of the  images
+  int spacing = 4;      // Spacing between elements
 
-    // Text size settings
-    carrier.display.setTextSize(2);
-    int charWidth = 6 * 2; 
-    int charHeight = 8 * 2; 
+  // Text size settings
+  carrier.display.setTextSize(2);
+  int charWidth = 6 * 2;
+  int charHeight = 8 * 2;
 
-    // Define the gradient color based on the time of day
-    int currentHour = timeClient.getHours();
-    uint8_t r, g, b;
-    for (int y = 0; y < displayHeight; y++) {
-        if (currentHour >= 6 && currentHour < 12) {
-            // Morning: Sunny gradient
-            greeting = "Good Morning";
-            r = map(y, 0, displayHeight - 1, 255, 255);
-            g = map(y, 0, displayHeight - 1, 200, 100);
-            b = map(y, 0, displayHeight - 1, 50, 0);
-        } else if (currentHour >= 12 && currentHour <= 18) {
-            // Afternoon: Orange gradient
-            greeting = "Good Afternoon";
-            r = map(y, 0, displayHeight - 1, 255, 204);
-            g = map(y, 0, displayHeight - 1, 128, 50);
-            b = map(y, 0, displayHeight - 1, 0, 0);
-        } else {
-            // Evening/Night: Darker gradient
-            greeting = "Good Evening";
-            r = map(y, 0, displayHeight - 1, 0, 50);
-            g = map(y, 0, displayHeight - 1, 0, 50);
-            b = map(y, 0, displayHeight - 1, 100, 150);
-        }
-        carrier.display.drawFastHLine(0, y, displayWidth, carrier.display.color565(r, g, b));
+  // Define the gradient color based on the time of day
+  int currentHour = timeClient.getHours();
+  Serial.println(currentHour);
+  uint8_t r, g, b;
+  for (int y = 0; y < displayHeight; y++) {
+    if (currentHour >= 6 && currentHour < 12) {
+      // Morning: Sunny gradient
+      greeting = "Good Morning";
+      r = map(y, 0, displayHeight - 1, 255, 255);
+      g = map(y, 0, displayHeight - 1, 200, 100);
+      b = map(y, 0, displayHeight - 1, 50, 0);
+    } else if (currentHour >= 12 && currentHour <= 18) {
+      // Afternoon: Orange gradient
+      greeting = "Good Afternoon";
+      r = map(y, 0, displayHeight - 1, 255, 204);
+      g = map(y, 0, displayHeight - 1, 128, 50);
+      b = map(y, 0, displayHeight - 1, 0, 0);
+    } else {
+      // Evening/Night: Darker gradient
+      greeting = "Good Evening";
+      r = map(y, 0, displayHeight - 1, 0, 50);
+      g = map(y, 0, displayHeight - 1, 0, 50);
+      b = map(y, 0, displayHeight - 1, 100, 150);
     }
+    carrier.display.drawFastHLine(0, y, displayWidth, carrier.display.color565(r, g, b));
+  }
+  int bitmapX = (displayWidth - bitmapSize) / 2;
+  int greetingY = 10 + bitmapSize + spacing; // Position for the greeting text below the bitmap
 
-    int yPos = 30; // Starting Y position
+  // Draw the appropriate bitmap for the time of day
+  if (currentHour >= 6 && currentHour < 12) {
+    carrier.display.drawBitmap(bitmapX, 10, m_sun, bitmapSize, bitmapSize, ST77XX_WHITE);
+  } else if (currentHour >= 12 && currentHour <= 18) {
+    carrier.display.drawBitmap(bitmapX, 10, m_sun_with_clouds, bitmapSize, bitmapSize, ST77XX_WHITE);
+  } else {
+    carrier.display.drawBitmap(bitmapX, 10, m_moon, bitmapSize, bitmapSize, ST77XX_WHITE);
+  }
 
-    // Draw greeting text
-    int textX = (displayWidth - greeting.length() * charWidth) / 2;
-    carrier.display.setCursor(textX, yPos);
-    carrier.display.println(greeting);
-    yPos += charHeight + spacing;
+  // Draw greeting text
+  int greetingX = (displayWidth - greeting.length() * charWidth) / 2;
+  carrier.display.setCursor(greetingX, greetingY);
+  carrier.display.println(greeting);
 
-    // Display date 
-    carrier.display.drawBitmap(10, yPos, m_calendar, bitmapSize, bitmapSize, ST77XX_WHITE);
-    textX = 10 + bitmapSize + 5;
-    carrier.display.setCursor(textX, yPos + (bitmapSize - charHeight) / 2);
-    carrier.display.println(currentDate);
-    yPos += bitmapSize + spacing;
+  int yPos = greetingY + charHeight + spacing; // Starting Y position for the icons and text
 
-    // Display time w
-    carrier.display.drawBitmap(10, yPos, m_clock, bitmapSize, bitmapSize, ST77XX_WHITE);
-    textX = 10 + bitmapSize + 5;
-    carrier.display.setCursor(textX, yPos + (bitmapSize - charHeight) / 2);
-    carrier.display.println(currentTime);
-    yPos += bitmapSize + spacing;
+  // Constants for layout
+  int iconWidth = bitmapSize; // Width of icons
+  int gapBetweenIconAndText = 5; // Gap between the icon and the text
 
-    // Display temperature 
-    String tempString = String(temperature) + " C";
-    carrier.display.drawBitmap(10, yPos, m_thermostat, bitmapSize, bitmapSize, ST77XX_WHITE);
-    textX = 10 + bitmapSize + 5;
-    carrier.display.setCursor(textX, yPos + (bitmapSize - charHeight) / 2);
-    carrier.display.println(tempString);
-    yPos += bitmapSize + spacing;
+  // Iterate over each item (date, time, temperature, humidity)
+  String items[] = { currentDate, currentTime, String(temperature) + " C", "Hum: " + String(humidity) + "%" };
+  const uint8_t* icons[] = { m_calendarx32, m_clockx32, m_thermostatx32, m_humidityx32 };
 
-    // Display humidity 
-    String humidityString = "Hum: " + String(humidity) + "%";
-    carrier.display.drawBitmap(10, yPos, m_humidity, bitmapSize, bitmapSize, ST77XX_WHITE);
-    textX = 10 + bitmapSize + 5;
-    carrier.display.setCursor(textX, yPos + (bitmapSize - charHeight) / 2);
-    carrier.display.println(humidityString);
+  for (int i = 0; i < 4; i++) {
+    String item = items[i];
+    int textWidth = item.length() * charWidth; // Estimated text width
+    int totalWidth = iconWidth + gapBetweenIconAndText + textWidth; // Total width of the row
+    int startX = (displayWidth - totalWidth) / 2; // Starting X position to center the row
 
-    displayActive = true; // Set the display active flag
-    displayStartMillis = millis(); // Record the start time
+    // Draw icon and text
+    carrier.display.drawBitmap(startX, yPos, icons[i], iconWidth, iconWidth, ST77XX_WHITE);
+    carrier.display.setCursor(startX + iconWidth + gapBetweenIconAndText, yPos + (iconWidth - charHeight) / 2);
+    carrier.display.println(item);
+
+    yPos += iconWidth + spacing; // Update yPos for the next item
+  }
+
+  displayActive = true; // Set the display active flag
+  displayStartMillis = millis(); // Record the start time
 }
-
